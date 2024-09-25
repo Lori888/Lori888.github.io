0ab5bd055eb219e488a25f1cb70f41af6020a170
luolp <luolp@asiainfo.com>
2024-09-25 17:21:07 +0800

    Site updated: 2024-09-25 17:20:59

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/08/23/Hexo建站笔记1/","2024/08/23/Hexo建站笔记2/","2024/09/10/Hexo建站笔记3/","2024/09/23/画境播放器/"];function toRandomPost(){
+var posts=["2024/08/23/Hexo建站笔记1/","2024/08/23/Hexo建站笔记2/","2024/09/10/Hexo建站笔记3/","2024/09/25/Android系统xapk安装方法/","2024/09/23/画境播放器/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
