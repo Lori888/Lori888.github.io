8357377fe8f951029f468bba074833282ad90136
luolp <luolp@asiainfo.com>
2024-10-09 18:11:27 +0800

    Site updated: 2024-10-09 18:11:17

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/10/08/NewPipe/","2024/09/27/NotionNext搭建博客/","2024/09/25/Android系统xapk安装方法/","2024/09/23/画境播放器/","2024/09/10/Hexo搭建博客3/","2024/08/23/Hexo搭建博客1/","2024/08/23/Hexo搭建博客2/"];function toRandomPost(){
+var posts=["2024/10/09/Windows10/","2024/10/08/NewPipe/","2024/09/27/NotionNext搭建博客/","2024/09/25/Android系统xapk安装方法/","2024/09/23/画境播放器/","2024/09/10/Hexo搭建博客3/","2024/08/23/Hexo搭建博客1/","2024/08/23/Hexo搭建博客2/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
